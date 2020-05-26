d15fa46c83212c5afa0b3e361423439fd5d21729
Alex Grecu <alexxwgrecw@gmail.com>
2020-05-26 20:53:13 +0300

    save

--- something.js
+++ something.js
@@ -1,84 +1,90 @@
+function ready(callback){
+  // in case the document is already rendered
+  if (document.readyState!='loading') callback();
+  // modern browsers
+  else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+  // IE <= 8
+  else document.attachEvent('onreadystatechange', function(){
+      if (document.readyState=='complete') callback();
+  });
+}
+
+
+
 //*****************MOVIES************/
+ready(function(){
+  // Select the carousel you'll need to manipulate and the buttons you'll add events to
+  const carousel = document.querySelector("[data-target='carousel']");
+  const card = carousel.getElementsByClassName("card");
+  const leftButton = document.querySelector("[data-action='slideLeft']");
+  const rightButton = document.querySelector("[data-action='slideRight']");
 
-// Select the carousel you'll need to manipulate and the buttons you'll add events to
-const carousel = document.querySelector("[data-target='carousel']");
-const card = carousel.querySelector("[data-target='card']");
-const leftButton = document.querySelector("[data-action='slideLeft']");
-const rightButton = document.querySelector("[data-action='slideRight']");
+  // Prepare to limit the direction in which the carousel can slide, 
+  // and to control how much the carousel advances by each time.
+  // In order to slide the carousel so that only three cards are perfectly visible each time,
+  // you need to know the carousel width, and the margin placed on a given card in the carousel;
+  //const cardStyle = card.currentStyle || window.getComputedStyle(card)
 
-// Prepare to limit the direction in which the carousel can slide, 
-// and to control how much the carousel advances by each time.
-// In order to slide the carousel so that only three cards are perfectly visible each time,
-// you need to know the carousel width, and the margin placed on a given card in the carousel
-const carouselWidth = carousel.offsetWidth;
-//const cardStyle = card.currentStyle || window.getComputedStyle(card)
-const cardMarginRight = 48;
-//asd
-// Count the number of total cards you have
-const cardCount = carousel.querySelectorAll("[data-target='card']").length;
+  // Define an offset property to dynamically update by clicking the button controls
+  // as well as a maxX property so the carousel knows when to stop at the upper limit
+  let offset = 0;
 
-// Define an offset property to dynamically update by clicking the button controls
-// as well as a maxX property so the carousel knows when to stop at the upper limit
-let offset = 0;
-const maxX = -((cardCount / 3) * carouselWidth + 
-               (cardMarginRight * (cardCount / 3)) - 
-               carouselWidth - cardMarginRight);
-
-// Add the click events
-leftButton.addEventListener("click", function() {
-  if (offset !== 0) {
-    offset += 234;
-    carousel.style.transform = `translateX(${offset}px)`;
+  // Add the click events
+  leftButton.addEventListener("click", function() {
+    if (offset !== 0) {
+      offset += 234;
+      for(var x = 0;x<card.length;x++){
+        card[x].style.transform = `translateX(${offset}px)`;
+      }
     }
-})
-  
-rightButton.addEventListener("click", function() {
-  if (offset !== maxX) {
+  })
+    
+  rightButton.addEventListener("click", function() {
     offset -= 234;
-    carousel.style.transform = `translateX(${offset}px)`;
-  }
-})
+    for(var x = 0;x<card.length;x++){
+      card[x].style.transform = `translateX(${offset}px)`;
+    }
+    
+  })
 
 
 
 //*****************TV SHOWS************/
 
 
-// Select the carousel you'll need to manipulate and the buttons you'll add events to
-const carousel2 = document.querySelector("[data-target='carousel2']");
-const card2 = carousel2.querySelector("[data-target='card2']");
-const leftButton2 = document.querySelector("[data-action='slideLeft2']");
-const rightButton2 = document.querySelector("[data-action='slideRight2']");
+  // Select the carousel you'll need to manipulate and the buttons you'll add events to
+  const carousel2 = document.querySelector("[data-target='carousel2']");
+  const card2 = carousel2.getElementsByClassName("card2");
+  const leftButton2 = document.querySelector("[data-action='slideLeft2']");
+  const rightButton2 = document.querySelector("[data-action='slideRight2']");
 
-// Prepare to limit the direction in which the carousel can slide, 
-// and to control how much the carousel advances by each time.
-// In order to slide the carousel so that only three cards are perfectly visible each time,
-// you need to know the carousel width, and the margin placed on a given card in the carousel
-const carouselWidth2 = carousel2.offsetWidth;
-//const cardStyle2 = card2.currentStyle || window.getComputedStyle(card2)
-const cardMarginRight2 = 48;
+  // Prepare to limit the direction in which the carousel can slide, 
+  // and to control how much the carousel advances by each time.
+  // In order to slide the carousel so that only three cards are perfectly visible each time,
+  // you need to know the carousel width, and the margin placed on a given card in the carousel
 
-// Count the number of total cards you have
-const cardCount2 = carousel2.querySelectorAll("[data-target='card2']").length;
+  // Define an offset property to dynamically update by clicking the button controls
+  // as well as a maxX property so the carousel knows when to stop at the upper limit
+  let offset2 = 0;
 
-// Define an offset property to dynamically update by clicking the button controls
-// as well as a maxX property so the carousel knows when to stop at the upper limit
-let offset2 = 0;
-const maxX2 = -((cardCount2 / 3) * carouselWidth2 + 
-               (cardMarginRight2 * (cardCount2 / 3)) - 
-               carouselWidth2 - cardMarginRight2);
-
-// Add the click events
-leftButton2.addEventListener("click", function() {
-  if (offset2 !== 0) {
-    offset2 += 234;
-    carousel2.style.transform = `translateX(${offset2}px)`;
+  // Add the click events
+  leftButton2.addEventListener("click", function() {
+    if (offset2 !== 0) {
+      offset2 += 234;
+      for(var x = 0;x<card2.length;x++){
+        card2[x].style.transform = `translateX(${offset2}px)`;
+      }
     }
-})
-  
-rightButton2.addEventListener("click", function() {
-  if (offset2 !== maxX2) {
+  })
+    
+  rightButton2.addEventListener("click", function() {
     offset2 -= 234;
-    carousel2.style.transform = `translateX(${offset2}px)`;
-  }
+    for(var x = 0;x<card2.length;x++){
+      card2[x].style.transform = `translateX(${offset2}px)`;
+    }
+    
+  })
+
 })
+
+
